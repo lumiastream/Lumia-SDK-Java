80f362162d42127a4fde8b531c22204455de5fad
Mumuney Abdulquadri <abdlquadri@googlemail.com>
2022-02-02 17:27:27 +0100

    refactor: Change System.out.println for a Logger

--- src/main/java/example/LumiaExample.java
+++ src/main/java/example/LumiaExample.java
@@ -10,9 +10,14 @@ import com.lumiastream.common.Rgb;
 import com.lumiastream.common.enums.LumiaAlertValue;
 import com.lumiastream.common.enums.LumiaExternalActivityCommandType;
 import com.lumiastream.common.enums.Platform;
+import io.vertx.mutiny.core.buffer.Buffer;
 import java.time.Duration;
+import java.util.logging.Logger;
 
 public class LumiaExample {
+
+  private static final Logger logger = Logger.
+      getLogger(LumiaExample.class.getCanonicalName());
 
   public static void main(String... args) {
     final Lumia client = Lumia
@@ -20,78 +25,74 @@ public class LumiaExample {
             new ConnectionOptions().setHost("127.0.0.1").setPort(39231).setName("lumia-java-sdk")
                 .setToken("insert-token"));
     client.connect().await().indefinitely();
-    client.getInfo().subscribe().with(System.out::println);
+    client.getInfo().subscribe().with(jsonObject -> logger.info(jsonObject::encode));
 
     // Send an alert
-    client.sendAlert(LumiaAlertValue.TWITCH_FOLLOWER).subscribe().with(System.out::println);
+    client.sendAlert(LumiaAlertValue.TWITCH_FOLLOWER).subscribe()
+        .with(jsonObject -> logger.info(jsonObject::encode));
 
     // Sending a command
-    client.sendCommand("red", false, false).subscribe().with(System.out::println);
+    client.sendCommand("red", false, false).subscribe()
+        .with(jsonObject -> logger.info(jsonObject::encode));
 
     // Sending a command
-    client.sendCommand("red", false, false).subscribe().with(System.out::println);
+    client.sendCommand("red", false, false).subscribe()
+        .with(jsonObject -> logger.info(jsonObject::encode));
 
     // Sending a basic color
     final MessageOptions messageOptions = new MessageOptions().setDuration(Duration.ofMillis(1000));
     client.sendColor(new Rgb(255, 0, 0), 60, messageOptions);
 
     // Sending a brightness
-    client.sendBrightness(100, new MessageOptions()).subscribe().with(System.out::println);
+    client.sendBrightness(100, new MessageOptions()).subscribe()
+        .with(jsonObject -> logger.info(jsonObject::encode));
 
     // Sending a TTS message
-    client.sendTts("This SDK is the best", 100, "").subscribe().with(System.out::println);
+    client.sendTts("This SDK is the best", 100, "").subscribe()
+        .with(jsonObject -> logger.info(jsonObject::encode));
 
     // Sending a Chat bot message
     client.sendChatBot(Platform.TWITCH, "This SDK is the best").subscribe()
-        .with(System.out::println);
+        .with(jsonObject -> logger.info(jsonObject::encode));
 
     // Sending a raw event example
     final LumiaPackParam lumiaPackParam = new LumiaPackParam();
     lumiaPackParam.setValue(LumiaAlertValue.TWITCH_FOLLOWER.getValue());
     final LumiaSendPack lumiaSendPack = new LumiaSendPack(LumiaExternalActivityCommandType.ALERT,
         lumiaPackParam);
-    client.send(lumiaSendPack).subscribe().with(System.out::println);
+    client.send(lumiaSendPack).subscribe().with(jsonObject -> logger.info(jsonObject::encode));
 
     // listen to events using the websocket handler directly
     client.getWebSocket().handler(buffer -> {
       final String type = buffer.toJsonObject().getString("type");
-      System.out.println("Incoming: " + type);
-      switch (type) {
-        case "states":
-          System.out.println("States have been updated: " + buffer.toJsonObject().encode());
-          break;
-        case "alert":
-          System.out.println("New alert: " + buffer.toJsonObject().encode());
-          break;
-        case "command":
-          System.out
-              .println("A Chat Command is being triggered: " + buffer.toJsonObject().encode());
-          break;
-        case "chat":
-          System.out.println("New chat message: " + buffer.toJsonObject().encode());
-          break;
-      }
+      output(buffer, type);
     });
 
     // listen to events using Mutiny Multi API
     client.getWebSocket().toMulti().subscribe().with(buffer -> {
       final String type = buffer.toJsonObject().getString("type");
-      System.out.println("Incoming: " + type);
-      switch (type) {
-        case "states":
-          System.out.println("States have been updated: " + buffer.toJsonObject().encode());
-          break;
-        case "alert":
-          System.out.println("New alert: " + buffer.toJsonObject().encode());
-          break;
-        case "command":
-          System.out
-              .println("A Chat Command is being triggered: " + buffer.toJsonObject().encode());
-          break;
-        case "chat":
-          System.out.println("New chat message: " + buffer.toJsonObject().encode());
-          break;
-      }
+      output(buffer, type);
     });
+  }
+
+  private static void output(Buffer buffer, String type) {
+    switch (type) {
+      case "states":
+        logger.info(
+            () -> String.format("States have been updated: %s", buffer.toJsonObject().encode()));
+        break;
+      case "alert":
+        logger.info(() -> String.format("New alert: %s", buffer.toJsonObject().encode()));
+        break;
+      case "command":
+        logger.info(() -> String
+            .format("A Chat Command is being triggered: %s", buffer.toJsonObject().encode()));
+        break;
+      case "chat":
+        logger.info(() -> String.format("New chat message: %s", buffer.toJsonObject().encode()));
+        break;
+      default:
+        logger.info(() -> "NO OP");
+    }
   }
 }
