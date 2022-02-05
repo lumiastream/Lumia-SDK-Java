6c8c23598c771bd00b7caed5775222c777b12e4a
Mumuney Abdulquadri <abdlquadri@googlemail.com>
2022-02-05 18:37:41 +0100

    refactor: use vertx API directly

--- src/main/java/com/lumiastream/client/Lumia.java
+++ src/main/java/com/lumiastream/client/Lumia.java
@@ -9,20 +9,20 @@ import com.lumiastream.common.Rgb;
 import com.lumiastream.common.enums.LumiaAlertValue;
 import com.lumiastream.common.enums.LumiaExternalActivityCommandType;
 import com.lumiastream.common.enums.Platform;
-import io.smallrye.mutiny.Multi;
-import io.smallrye.mutiny.Uni;
-import io.smallrye.mutiny.groups.UniOnFailure;
+import io.vertx.core.Handler;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.WebSocket;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.jackson.DatabindCodec;
-import io.vertx.mutiny.core.Vertx;
-import io.vertx.mutiny.core.buffer.Buffer;
-import io.vertx.mutiny.core.http.WebSocket;
-import java.util.concurrent.CountDownLatch;
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 public class Lumia {
 
+  HashMap<Integer, Handler<Buffer>> hashMap = new HashMap<Integer , Handler<Buffer>>();
   private WebSocket webSocket;
   private final ConnectionOptions lumiaOptions;
   private static final Logger logger = Logger
@@ -50,125 +50,130 @@ public class Lumia {
     prettyMapper.registerModule(new JavaTimeModule());
   }
 
-  public Uni<Boolean> connect(final boolean shouldAutoReconnect) {
+  public Promise<Boolean> connect(final boolean shouldAutoReconnect) {
+    final Promise<Boolean> result = Promise.promise();
     final StringBuilder uri = new StringBuilder().append("/api?token=")
         .append(lumiaOptions.getToken())
         .append("&name=").append(lumiaOptions.getName());
     logger.info(() -> String.format("Connecting:- URI: %s", uri));
 
-    final UniOnFailure<Boolean> onFailure = Vertx.vertx().createHttpClient()
+    Vertx.vertx().createHttpClient()
         .webSocket(lumiaOptions.getPort(), lumiaOptions.getHost(), uri.toString())
-        .onItem().transform(webSocket1 -> {
-          webSocket = webSocket1;
-          logger.info(() -> String.format("Connected:- Closed Status: %s", webSocket1.isClosed()));
-          return webSocket.isClosed();
+        .onSuccess(new Handler<WebSocket>() {
+          @Override
+          public void handle(WebSocket event) {
+            webSocket = event;
+            result.complete(event.isClosed());
+          }
         })
-        .onFailure(throwable -> {
-          throwable.printStackTrace();
-          return true;
+        .onFailure(new Handler<Throwable>() {
+          @Override
+          public void handle(Throwable event) {
+            event.printStackTrace();
+            result.complete(true);
+          }
         });
-    if (shouldAutoReconnect) {
-      return onFailure.retry().indefinitely();
-    } else {
-      return onFailure.recoverWithItem(true);
-    }
+    return result;
   }
 
-  public Multi<JsonObject> getInfo() {
+  public void getInfo(final Handler<Buffer> function) {
     final JsonObject getInfoPayload = new JsonObject().put("retrieve", true)
         .put("method", "retrieve");
     final Buffer buffer = Buffer.buffer(getInfoPayload.toString());
     logger.info(() -> String.format("Getting Info:- Data: %s", buffer.toString()));
-    return sendWebsocketMessage(buffer.toString());
+    sendWebsocketMessage(buffer.toJsonObject(),
+        function);
   }
 
-  public Multi<JsonObject> stop() {
+  public void stop(
+      final Handler<Buffer> function) {
     final JsonObject getInfoPayload = new JsonObject().put("method", "stop");
     final Buffer buffer = Buffer.buffer(getInfoPayload.toString());
     logger.info(() -> String.format("Stopping:- Data: %s", buffer.toString()));
-    return sendWebsocketMessage(buffer.toString());
+    sendWebsocketMessage(buffer.toJsonObject(),
+        function);
   }
 
-  private Multi<JsonObject> sendWebsocketMessage(final String json) {
-    final CountDownLatch countDownLatch = new CountDownLatch(1);
-    JsonObject result = new JsonObject();
+  private void sendWebsocketMessage(final JsonObject json, final Handler<Buffer> handler) {
+    final int context = handler.hashCode();
+    hashMap.put(context, handler);
+    json.put("context", context);
     if (webSocket != null) {
       logger.info(() -> String.format("Sending Websocket Message:- Data: %s: isClosed: %s"
           , json, webSocket.isClosed()));
-      webSocket.handler(buffer -> result.mergeIn(new JsonObject(buffer.toString())))
-          .write(Buffer.buffer(json));
-      countDownLatch.countDown();
-    } else {
-      result.put("message", "WebSocket not connected");
-      countDownLatch.countDown();
+      webSocket.handler(new Handler<Buffer>() {
+        @Override
+        public void handle(Buffer event) {
+          final Integer context = event.toJsonObject().getInteger("context");
+          hashMap.get(context).handle(event);
+          hashMap.remove(context);
+        }
+      }).write(json.toBuffer());
     }
-    try {
-      countDownLatch.await();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-    return Multi.createFrom().item(result);
   }
 
-  public Multi<JsonObject> send(final LumiaSendPack pack) {
+  public void send(final LumiaSendPack pack, final Handler<Buffer> function) {
+
     final String packString = Json.encode(pack);
     final JsonObject message = new JsonObject().put("lsorigin", "lumia-sdk");
     final JsonObject merged = message.mergeIn(new JsonObject(packString));
     logger.info(() -> String.format("Data: %s", merged.encode()));
-    return sendWebsocketMessage(merged.encode());
+    sendWebsocketMessage(merged, function);
   }
 
-  public Multi<JsonObject> sendAlert(final LumiaAlertValue alert) {
+  public void sendAlert(final LumiaAlertValue alert, final Handler<Buffer> function) {
     final LumiaPackParam packParam = new LumiaPackParam().setValue(alert.getValue());
     final LumiaSendPack pack = new LumiaSendPack(LumiaExternalActivityCommandType.ALERT, packParam);
     logger.info(() -> String.format("Alerting :- Data: %s", Json.encode(pack)));
-    return send(pack);
+    send(pack, function);
   }
 
-  public Multi<JsonObject> sendChatBot(final Platform platform, final String text) {
+  public void sendChatBot(final Platform platform, final String text,
+      final Handler<Buffer> function) {
     final LumiaPackParam packParam = new LumiaPackParam().setValue(text).setPlatform(platform);
     final LumiaSendPack pack = new LumiaSendPack(LumiaExternalActivityCommandType.CHATBOT_MESSAGE,
         packParam);
     logger.info(() -> String.format("Chat Botting :- Data: %s", Json.encode(pack)));
-    return send(pack);
+    send(pack, function);
   }
 
-  public Multi<JsonObject> sendColor(final Rgb rgb, final Integer brightness,
-      final MessageOptions messageOptions) {
+  public void sendColor(final Rgb rgb, final Integer brightness,
+      final MessageOptions messageOptions, final Handler<Buffer> function) {
     final LumiaPackParam packParam = new LumiaPackParam().setValue(Json.encode(rgb))
         .setBrightness(brightness).setHold(messageOptions.getHoldDefault());
     final LumiaSendPack pack = new LumiaSendPack(LumiaExternalActivityCommandType.RGB_COLOR,
         packParam);
     logger.info(() -> String.format("Coloring :- Data: %s", Json.encode(pack)));
-    return send(pack);
+    send(pack, function);
   }
 
-  public Multi<JsonObject> sendCommand(final String command, final Boolean hold,
-      final Boolean skipQueue) {
+  public void sendCommand(final String command, final Boolean hold,
+      final Boolean skipQueue, final Handler<Buffer> function) {
     final LumiaPackParam packParam = new LumiaPackParam().setValue(command).setHold(hold)
         .setSkipQueue(skipQueue);
     final LumiaSendPack pack = new LumiaSendPack(LumiaExternalActivityCommandType.CHAT_COMMAND,
         packParam);
     logger.info(() -> String.format("Commanding :- Data: %s", Json.encode(pack)));
-    return send(pack);
+    send(pack, function);
   }
 
-  public Multi<JsonObject> sendBrightness(final Integer brightness,
-      final MessageOptions messageOptions) {
+  public void sendBrightness(final Integer brightness,
+      final MessageOptions messageOptions, final Handler<Buffer> function) {
     final LumiaPackParam packParam = new LumiaPackParam().setBrightness(brightness)
         .setDuration(messageOptions.getDuration()).setTransition(messageOptions.getTransition());
     final LumiaSendPack pack = new LumiaSendPack(LumiaExternalActivityCommandType.RGB_COLOR,
         packParam);
     logger.info(() -> String.format("Brightness :- Data: %s", Json.encode(pack)));
-    return send(pack);
+    send(pack, function);
   }
 
-  public Multi<JsonObject> sendTts(final String text, final Integer volume, final String voice) {
+  public void sendTts(final String text, final Integer volume, final String voice,
+      final Handler<Buffer> function) {
     final LumiaPackParam packParam = new LumiaPackParam().setValue(text).setVolume(volume)
         .setVoice(voice);
     final LumiaSendPack pack = new LumiaSendPack(LumiaExternalActivityCommandType.TTS, packParam);
     logger.info(() -> String.format("TTSing :- Data: %s", Json.encode(pack)));
-    return send(pack);
+    send(pack, function);
   }
 
 }
--- src/main/java/example/LumiaExample.java
+++ src/main/java/example/LumiaExample.java
@@ -10,7 +10,8 @@ import com.lumiastream.common.Rgb;
 import com.lumiastream.common.enums.LumiaAlertValue;
 import com.lumiastream.common.enums.LumiaExternalActivityCommandType;
 import com.lumiastream.common.enums.Platform;
-import io.vertx.mutiny.core.buffer.Buffer;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
 import java.time.Duration;
 import java.util.logging.Logger;
 
@@ -24,54 +25,64 @@ public class LumiaExample {
         .getInstance(
             new ConnectionOptions().setHost("127.0.0.1").setPort(39231).setName("lumia-java-sdk")
                 .setToken("insert-token"));
-    client.connect(false).await().indefinitely();
-    client.getInfo().subscribe().with(jsonObject -> logger.info(jsonObject::encode));
+    client.connect(false).future().onSuccess(new Handler<Boolean>() {
+      @Override
+      public void handle(Boolean event) {
+        System.out.println("<<<<<<<<<<" + event);
 
-    // Send an alert
-    client.sendAlert(LumiaAlertValue.TWITCH_FOLLOWER).subscribe()
-        .with(jsonObject -> logger.info(jsonObject::encode));
 
-    // Sending a command
-    client.sendCommand("red", false, false).subscribe()
-        .with(jsonObject -> logger.info(jsonObject::encode));
+        // Send an alert
+        client.sendAlert(LumiaAlertValue.TWITCH_FOLLOWER,
+            data -> System.out.println("aaaaaaaaaalerrrrrrrrt"+data.toJsonObject()));
+        client.getInfo(data -> System.out.println("iiiiiiiiinffffffffffooooooooo"+data.toJsonObject()));
 
-    // Sending a command
-    client.sendCommand("red", false, false).subscribe()
-        .with(jsonObject -> logger.info(jsonObject::encode));
+        // Sending a command
+        client.sendCommand("red", false, false, data -> System.out.println("cccconnnnamd"+data.toJsonObject()));
 
-    // Sending a basic color
-    final MessageOptions messageOptions = new MessageOptions().setDuration(Duration.ofMillis(1000));
-    client.sendColor(new Rgb(255, 0, 0), 60, messageOptions);
+        // Sending a basic color
+        final MessageOptions messageOptions = new MessageOptions()
+            .setDuration(Duration.ofMillis(1000));
+        client.sendColor(new Rgb(255, 0, 0), 60, messageOptions,
+            data -> System.out.println("coooloooorrrrr"+data.toJsonObject()));
 
-    // Sending a brightness
-    client.sendBrightness(100, new MessageOptions()).subscribe()
-        .with(jsonObject -> logger.info(jsonObject::encode));
+        // Sending a brightness
+        client.sendBrightness(100, new MessageOptions(),
+            data -> System.out.println("brrritthgns"+data.toJsonObject()));
 
-    // Sending a TTS message
-    client.sendTts("This SDK is the best", 100, "").subscribe()
-        .with(jsonObject -> logger.info(jsonObject::encode));
+        // Sending a TTS message
+        client.sendTts("This SDK is the best", 100, "",
+            data -> System.out.println("tttttsssttttt"+data.toJsonObject()));
 
-    // Sending a Chat bot message
-    client.sendChatBot(Platform.TWITCH, "This SDK is the best").subscribe()
-        .with(jsonObject -> logger.info(jsonObject::encode));
+        // Sending a Chat bot message
+        client.sendChatBot(Platform.TWITCH, "This SDK is the best",
+            data -> System.out.println("chaaatttt"+data.toJsonObject()));
 
-    // Sending a raw event example
-    final LumiaPackParam lumiaPackParam = new LumiaPackParam();
-    lumiaPackParam.setValue(LumiaAlertValue.TWITCH_FOLLOWER.getValue());
-    final LumiaSendPack lumiaSendPack = new LumiaSendPack(LumiaExternalActivityCommandType.ALERT,
-        lumiaPackParam);
-    client.send(lumiaSendPack).subscribe().with(jsonObject -> logger.info(jsonObject::encode));
+        // Sending a raw event example
+        final LumiaPackParam lumiaPackParam = new LumiaPackParam();
+        lumiaPackParam.setValue(LumiaAlertValue.TWITCH_FOLLOWER.getValue());
+        final LumiaSendPack lumiaSendPack = new LumiaSendPack(
+            LumiaExternalActivityCommandType.ALERT,
+            lumiaPackParam);
+        client.send(lumiaSendPack, data -> System.out.println("senddddD\n"+data.toJsonObject()));
 
-    // listen to events using the websocket handler directly
-    client.getWebSocket().handler(buffer -> {
-      final String type = buffer.toJsonObject().getString("type");
-      output(buffer, type);
-    });
+        // listen to events using the websocket handler directly
+//    client.getWebSocket().handler(buffer -> {
+//      System.out.println(buffer.toJsonObject().encode());
+//      final String type = buffer.toJsonObject().getString("type");
+//      output(buffer, type);
+//    }).exceptionHandler(new Handler<Throwable>() {
+//      @Override
+//      public void handle(Throwable event) {
+//        event.printStackTrace();
+//      }
+//    });
 
-    // listen to events using Mutiny Multi API
-    client.getWebSocket().toMulti().subscribe().with(buffer -> {
-      final String type = buffer.toJsonObject().getString("type");
-      output(buffer, type);
+      }
+    }).onFailure(new Handler<Throwable>() {
+      @Override
+      public void handle(Throwable event) {
+        event.printStackTrace();
+      }
     });
   }
 
