c118d0906cc6b5d2b355175e2c15f921250b625d
AhadCove <ahadcove@gmail.com>
2022-02-14 21:07:50 -0500

    feat: signing complete

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,111 +1,37 @@
-import java.util.Base64.getDecoder
+import com.vanniktech.maven.publish.SonatypeHost
 
 plugins {
   `java-library`
-  `maven-publish`
-  signing
+  id("com.vanniktech.maven.publish") version "0.17.0"
+  id("org.jetbrains.dokka") version "1.6.10"
 }
 
 group = "com.lumiastream"
-version = "0.1.0-SNAPSHOT"
 
-repositories {
-  mavenCentral()
-}
+version = "0.1.0"
 
-val vertxVersion = "4.2.4"
-val jacksonVersion = "2.13.0"
-val junitJupiterVersion = "5.7.0"
-
-val watchForChange = "src/**/*"
-val doOnChange = "${projectDir}/gradlew classes"
+repositories { mavenCentral() }
 
 dependencies {
-  implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
+  implementation(platform("io.vertx:vertx-stack-depchain:4.2.4"))
   implementation("io.vertx:vertx-core")
-  implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+  implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.0")
   testImplementation("io.vertx:vertx-junit5")
-  testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
+
+  // Signing
+  implementation("com.vanniktech:gradle-maven-publish-plugin:0.17.0")
 }
 
 java {
   sourceCompatibility = JavaVersion.VERSION_1_8
   targetCompatibility = JavaVersion.VERSION_1_8
-
-  withJavadocJar()
-  withSourcesJar()
 }
 
 tasks.withType<Test> {
   useJUnitPlatform()
-  testLogging {
-  }
+  testLogging {}
 }
 
-publishing {
-  publications {
-    create<MavenPublication>("mavenJava") {
-      artifactId = "lumiastream-websocket-sdk"
-      from(components["java"])
-      versionMapping {
-        usage("java-api") {
-          fromResolutionOf("runtimeClasspath")
-        }
-        usage("java-runtime") {
-          fromResolutionResult()
-        }
-      }
-      pom {
-        name.set("Lumiastream Websocket SDK")
-        description.set("A java websocket library for Lumiastream")
-        url.set("http://www.github.com/library")
-        properties.set(mapOf(
-        ))
-        licenses {
-          license {
-            name.set("The Apache License, Version 2.0")
-            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-          }
-        }
-        developers {
-          developer {
-            id.set("johnd")
-            name.set("John Doe")
-            email.set("john.doe@github.com")
-          }
-        }
-        scm {
-          connection.set("scm:git:git://github.com/lumiastream-websocket-sdk.git")
-          developerConnection.set("scm:git:ssh://github.com/lumiastream-websocket-sdk.git")
-          url.set("http://github.com/lumiastream-websocket-sdk/")
-        }
-      }
-    }
-  }
-  repositories {
-    maven {
-      // change URLs to point to your repos, e.g. http://my.org/repo
-      val releasesRepoUrl = uri(layout.buildDirectory.dir("repos/releases"))
-      val snapshotsRepoUrl = uri(layout.buildDirectory.dir("repos/snapshots"))
-      url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-    }
-  }
-}
-
-//fun base64Decode(prop: String): String? {
-//  return project.findProperty(prop)?.let {
-//    String(getDecoder().decode(it.toString())).trim()
-//  }
-//}
-//
-//signing {
-//  useInMemoryPgpKeys(base64Decode("signingKey"), base64Decode("signingPassword"))
-//  sign(publishing.publications["mavenJava"])
-//}
-
-tasks.javadoc {
-  if (JavaVersion.current().isJava9Compatible) {
-    (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
-  }
-}
+mavenPublish { sonatypeHost = SonatypeHost.S01 }
